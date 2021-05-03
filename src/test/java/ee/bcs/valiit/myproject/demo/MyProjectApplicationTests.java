d8e75a0877a73829008bbad7bc788623c4e13949
Kelly Miadzieles <kellymiadzieles@gmail.com>
2021-05-03 10:28:00 +0300

    Initial commit

---
+++ src/test/java/ee/bcs/valiit/myproject/demo/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.bcs.valiit.myproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
