b6f68ea9cbedcc3a140365054a1bd3cd006d931f
Kadri Green <kadrigr@gmail.com>
2021-05-07 16:26:14 +0300

    Commit

--- src/main/java/ee/bcs/valiit/myproject/demo/myproject/CreateAdvertisementController.java
+++ src/main/java/ee/bcs/valiit/myproject/demo/myproject/CreateAdvertisementController.java
@@ -20,7 +20,7 @@ public class CreateAdvertisementController {
         createAdvertisementService.createAdvertisement(advertisementDTO);
     }
 
-   @GetMapping("/getAdsByLocation/{location}")
+    @GetMapping("/getAdsByLocation/{location}")
     public List<AdvertisementDTO> getAdsByLocation(@PathVariable("location") String location) {
         return createAdvertisementService.getAdsByLocation(location);
     }
@@ -36,26 +36,28 @@ public class CreateAdvertisementController {
     }
 
 
-    @GetMapping ("/getAdsByPrice/{priceFrom}/{priceTo}")
-    public List<AdvertisementDTO> getAdsByPrice (@PathVariable("priceFrom") Double priceFrom,
-                                                 @PathVariable ("priceTo") Double priceTo){
+    @GetMapping("/getAdsByPrice/{priceFrom}/{priceTo}")
+    public List<AdvertisementDTO> getAdsByPrice(@PathVariable("priceFrom") Double priceFrom,
+                                                @PathVariable("priceTo") Double priceTo) {
         return createAdvertisementService.getAdsByPrice(priceFrom, priceTo);
     }
 
     @GetMapping("/getAdsByCategory/{category}")
-    public List <AdvertisementDTO> getAdsByCategory(@PathVariable("category") String category){
+    public List<AdvertisementDTO> getAdsByCategory(@PathVariable("category") String category) {
         return createAdvertisementService.getAdsByCategory(category);
     }
+
     @GetMapping("/filterAdsByPriceCategoryLocation/")
     public List<AdvertisementDTO> filterAdsByPriceCategoryLocation(@RequestParam(value = "a", required = false) String category,
                                                                    @RequestParam(value = "b", required = false) String location,
                                                                    @RequestParam(value = "c", required = false) Double priceFrom,
-                                                                   @RequestParam(value = "d", required = false) Double priceTo) {
-        return createAdvertisementService.filterAdsByPriceCategoryLocation(category, location, priceFrom, priceTo);
+                                                                   @RequestParam(value = "d", required = false) Double priceTo,
+                                                                   @RequestParam(value = "e", required = false) String input) {
+        return createAdvertisementService.filterAdsByPriceCategoryLocation(category, location, priceFrom, priceTo, input);
     }
 
-    @GetMapping ("/searchAdsByTitleDescription/{input}")
-    public List<AdvertisementDTO> searchAdsByTitleDescription (@PathVariable("input") String input){
+    @GetMapping("/searchAdsByTitleDescription/{input}")
+    public List<AdvertisementDTO> searchAdsByTitleDescription(@PathVariable("input") String input) {
         return createAdvertisementService.searchAdsByTitleDescription(input);
     }
 
