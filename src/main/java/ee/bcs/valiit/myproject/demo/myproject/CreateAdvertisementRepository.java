efdcbad790478b8468ea2f174e9db0f72ba725c0
IngaS13 <inga.sokman@gmail.com>
2021-05-07 10:49:22 +0300

    7. mai

--- src/main/java/ee/bcs/valiit/myproject/demo/myproject/CreateAdvertisementRepository.java
+++ src/main/java/ee/bcs/valiit/myproject/demo/myproject/CreateAdvertisementRepository.java
@@ -3,6 +3,8 @@ package ee.bcs.valiit.myproject.demo.myproject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.HashMap;
 import java.util.List;
@@ -88,4 +90,5 @@ public class CreateAdvertisementRepository {
         }
         return jdbcTemplate.query(sql, paramMap, new AdvertisementRowMapper());
     }
+
 }
